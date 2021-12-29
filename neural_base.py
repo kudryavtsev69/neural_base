eb53a7a556ba50c091503237fed92825c7d5dbb7
kudry <kudryavtsev69@gmail.com>
2021-12-29 19:53:49 +0300

    Работает

--- neural_base.py
+++ neural_base.py
@@ -1,21 +1,24 @@
 import numpy as np
 
 def sigmoid(x):   # Формула расчета порогвых значений (сигмоида)
-    return 1/(1 + np.e)
+    return 1/(1 + np.exp(-x))
 
-training_inputs = np.array([[0,0,1], [1,1,1], [1,0,1], [0,1,1]])
+training_inputs = np.array([[0,0,1],
+                            [1,1,1],
+                            [1,0,1],
+                            [0,1,1]])
 
 training_outputs = np.array([[0, 1, 1, 0]]).T  # Значения ответов
 
 np.random.seed(1)   # Генератор случайных значений
 
-synaptic_weights = 2 * np.random.random((3,1)) - 1   # Определение весов
+synaptic_weights = 2 * np.random.random((3, 1)) - 1   # Определение весов
 
 print('Случайно инициализированные веса: ')
 print(synaptic_weights)
 
 # Метод обратного распространения
-for i in range(20000):
+for i in range(200):
     input_layer = training_inputs
     outputs = sigmoid(np.dot(input_layer, synaptic_weights))
 
